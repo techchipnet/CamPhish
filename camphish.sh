35006d81b256048a2aae0fdd9a83bf5b766c7037
om000 <71024183+om000@users.noreply.github.com>
2020-09-10 16:08:50 +0530

    Ok
    Okay

--- camphish.sh
+++ camphish.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # CamPhish v1.0
-# Powered by TechChip
+# Powered by Cyber Alert 07
 # Credits goes to thelinuxchoice [github.com/thelinuxchoice/]
 
 trap 'printf "\n";stop' 2
 
 banner() {
 clear
-printf "\e[1;92m  _______  _______  _______  \e[0m\e[1;77m_______          _________ _______          \e[0m\n"
-printf "\e[1;92m (  ____ \(  ___  )(       )\e[0m\e[1;77m(  ____ )|\     /|\__   __/(  ____ \|\     /|\e[0m\n"
-printf "\e[1;92m | (    \/| (   ) || () () |\e[0m\e[1;77m| (    )|| )   ( |   ) (   | (    \/| )   ( |\e[0m\n"
-printf "\e[1;92m | |      | (___) || || || |\e[0m\e[1;77m| (____)|| (___) |   | |   | (_____ | (___) |\e[0m\n"
-printf "\e[1;92m | |      |  ___  || |(_)| |\e[0m\e[1;77m|  _____)|  ___  |   | |   (_____  )|  ___  |\e[0m\n"
-printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | |         ) || (   ) |\e[0m\n"
-printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
-printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n\n"
+printf "\e[1;92m  _______  _______  \e[0m\e[1;77m\n"
+printf "\e[1;92m (  ____ )(       )\e[0m\e[1;7m\n"
+printf "\e[1;92m | (    ) | () () |\e[0m\e[1;77m\n"
+printf "\e[1;92m | |    | | || || || |\e[0m\e[1;77m\n"
+printf "\e[1;92m | |    | | | |(_)| |\e[0m\e[1;77m\n"
+printf "\e[1;92m | |    | | | |   | |\e[0m\e[1;77m\n"
+printf "\e[1;92m | (___)| | )   ( |\e[0m\e[1;77m\n"
+printf "\e[1;92m (_______)|/     \|\e[0m\e[1;77m\n"
 
-printf " \e[1;77m www.techchip.net | youtube.com/techchipnet \e[0m \n"
+printf " \e | youtube.com/cyber alert 07 \e[0m \n"
 
 printf "\n"
 
