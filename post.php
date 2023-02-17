d9d6145c79a5435453fdeb5615c6d5db833f30a3
krit <krittakon159@gmail.com>
2023-02-17 17:08:53 +0700

    config output dir

--- post.php
+++ post.php
@@ -2,6 +2,7 @@
 
 $date = date('dMYHis');
 $imageData=$_POST['cat'];
+$output_dir="out/";
 
 if (!empty($_POST['cat'])) {
 error_log("Received" . "\r\n", 3, "Log.log");
@@ -10,7 +11,12 @@ error_log("Received" . "\r\n", 3, "Log.log");
 
 $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
+
+if (!file_exists($output_dir)) {
+    mkdir($output_dir, 0777, true);
+}
+
+$fp = fopen( $output_dir.'cam'.$date.'.png', 'wb' );
 fwrite( $fp, $unencodedData);
 fclose( $fp );
 
