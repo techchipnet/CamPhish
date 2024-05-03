8160a9ca8b5dec512c75711afd088dc234c46e5f
TechChip <42796435+techchipnet@users.noreply.github.com>
2024-05-03 22:02:27 +0530

    Update Ver 1.7
    Fix and add support for apple silicon chip and raspberry pi

--- camphish.sh
+++ camphish.sh
@@ -178,7 +178,9 @@ command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not inst
 printf "\e[1;92m[\e[0m+\e[1;92m] Downloading Ngrok...\n"
 arch=$(uname -a | grep -o 'arm' | head -n1)
 arch2=$(uname -a | grep -o 'Android' | head -n1)
-if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
+arch3=$(uname -a | grep -o 'aarch64' | head -n1)
+arch4=$(uname -a | grep -o 'Darwin' | head -n1)
+if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] && [[ $arch4 != *'Darwin'* ]] ; then
 wget --no-check-certificate https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.zip > /dev/null 2>&1
 
 if [[ -e ngrok-v3-stable-linux-arm.zip ]]; then
@@ -187,6 +189,28 @@ chmod +x ngrok
 rm -rf ngrok-v3-stable-linux-arm.zip
 else
 printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+exit 1
+fi
+elif [[ $arch3 == *'aarch64'* ]] ; then
+wget --no-check-certificate https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.zip > /dev/null 2>&1
+ 
+if [[ -e ngrok-v3-stable-linux-arm64.zip ]]; then
+unzip ngrok-v3-stable-linux-arm64.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-v3-stable-linux-arm64.zip
+else
+printf "\e[1;93m[!] Download error...\e[0m\n"
+exit 1
+fi
+elif [[ $arch4 == *'Darwin'* ]] ; then
+wget --no-check-certificate https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-darwin-arm64.zip> /dev/null 2>&1
+ 
+if [[ -e ngrok-v3-stable-darwin-arm64.zip ]]; then
+unzip ngrok-v3-stable-darwin-arm64.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-v3-stable-darwin-arm64.zip
+else
+printf "\e[1;93m[!] Download error...\e[0m\n"
 exit 1
 fi
 
